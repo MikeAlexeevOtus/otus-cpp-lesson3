0493d950e9648a4f2949c244776fd56a2a5ccf3d
MikeAlexeevOtus <59478759+MikeAlexeevOtus@users.noreply.github.com>
2020-01-03 21:52:51 +0300

    Initial commit

---
+++ src/hello.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+#include "lib.h"
+
+int main() {
+    std::cout << "Hello, World! version: " << hello::version() << std::endl;
+    return 0;
+}
---
+++ src/lib.h
@@ -0,0 +1,5 @@
+#pragma once
+
+namespace hello {
+    int version();
+}
