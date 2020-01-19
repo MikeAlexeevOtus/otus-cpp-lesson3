f498b1bbdba298bf4baf77cf1fa55a2c8a231ad5
MikeAlexeevOtus <mike.42.alexeev@yandex.ru>
2020-01-19 09:46:52 +0300

    uniq nodes for last byte only

--- prototype.py
+++ prototype.py
@@ -3,24 +3,22 @@ import random
 from pprint import pprint
 
 
-def make_node(key):
-    # make node uniq
-    # to allow equal keys in dict
+def make_unique_node(key):
+    # allow equal keys in dict
     return key, random.random()
 
 
 def insert_ip_as_list(ip_as_list, ip_tree):
     prefix = ip_as_list.pop(0)
-    node = make_node(prefix)
     if not ip_as_list:   # terminate tree
-        ip_tree[node] = None
+        ip_tree[prefix] = None
         return
 
     # init branch in tree
     if prefix not in ip_tree:
-        ip_tree[node] = {}
+        ip_tree[prefix] = {}
 
-    insert_ip_as_list(ip_as_list, ip_tree[node])
+    insert_ip_as_list(ip_as_list, ip_tree[prefix])
 
 
 def load_data(file_path):
@@ -31,18 +29,27 @@ def load_data(file_path):
             line = line.rstrip()
             ip, _, _ = line.split()
             ip_as_list = [int(part) for part in ip.split('.')]
+            ip_as_list[3] = make_unique_node(ip_as_list[3])  # allow dups
             insert_ip_as_list(ip_as_list, ip_tree)
 
     return ip_tree
 
 
+def print_addr(addr_parts):
+    # last part is stored as pair (value, random)
+    # retrieve value
+    addr_parts = list(addr_parts)
+    addr_parts[3] = addr_parts[3][0]
+    print('.'.join(str(b) for b in addr_parts))
+
+
 def print_tree_sorted(accumulated_prefixes, ip_tree):
     if not ip_tree:   # leave case
-        print('.'.join(str(b) for b in accumulated_prefixes))
+        print_addr(accumulated_prefixes)
         return
 
-    for node, subtree in sorted(ip_tree.items(), reverse=True):
-        print_tree_sorted(accumulated_prefixes + [node[0]], subtree)
+    for prefix, subtree in sorted(ip_tree.items(), reverse=True):
+        print_tree_sorted(accumulated_prefixes + [prefix], subtree)
 
 
 ip_tree = load_data(sys.argv[1])
