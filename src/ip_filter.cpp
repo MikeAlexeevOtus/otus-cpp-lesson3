63df23d09e7e8f0ab0a457638f53296a88b8bd95
MikeAlexeevOtus <mike.42.alexeev@yandex.ru>
2020-01-25 13:42:37 +0300

    improved types, added filter arg, use lambdas

--- src/ip_filter.cpp
+++ src/ip_filter.cpp
@@ -1,4 +1,5 @@
 #include <algorithm>
+#include <functional>
 #include <array>
 #include <cstdint>
 #include <iostream>
@@ -8,11 +9,12 @@
 const char COLS_DELIM = '\t';
 const char ADDR_DELIM = '.';
 
-using IpSplitted = std::vector<uint8_t>;
-using IpList = std::vector<IpSplitted>;
+using IpByte = uint8_t;
+using IpAddr = std::vector<IpByte>;
+using IpList = std::vector<IpAddr>;
 
 
-void print_addr(const IpSplitted &addr) {
+void print_addr(const IpAddr &addr) {
     for(auto it = addr.cbegin(); it != addr.cend(); it++) {
         if (it != addr.cbegin()) {
             // prepend with dot starting from the second byte
@@ -23,14 +25,16 @@ void print_addr(const IpSplitted &addr) {
     std::cout << std::endl;
 }
 
-void print_addr_list(const IpList &ip_list) {
+void print_addr_list(const IpList &ip_list,
+                     std::function<bool(const IpAddr&)> test_function) {
     for (auto it = ip_list.cbegin(); it != ip_list.cend(); it++) {
-        print_addr(*it);
+        if (test_function(*it))
+            print_addr(*it);
     }
 }
 
-IpSplitted parse_addr(const std::string &addr_str) {
-    IpSplitted parsed_addr;
+IpAddr parse_addr(const std::string &addr_str) {
+    IpAddr parsed_addr;
     std::string::size_type start = 0, stop = 0;
 
     while(stop != std::string::npos) {
@@ -45,15 +49,28 @@ IpSplitted parse_addr(const std::string &addr_str) {
 }
 
 
+bool any_is_equal(const IpAddr &ip_addr, IpByte value) {
+    return std::any_of(
+            ip_addr.cbegin(),
+            ip_addr.cend(),
+            [value](IpByte ip_byte) { return ip_byte == value; }
+        );
+}
+
+
 int main() {
     IpList ip_list;
 
     for(std::string line; std::getline(std::cin, line);) {
+        // cut first column
         std::string addr = line.substr(0, line.find(COLS_DELIM));
         ip_list.push_back(parse_addr(addr));
     }
 
-    std::sort(ip_list.begin(), ip_list.end(), std::greater<IpSplitted>());
+    std::sort(ip_list.begin(), ip_list.end(), std::greater<IpAddr>());
 
-    print_addr_list(ip_list);
+    print_addr_list(ip_list, [](const IpAddr &ip_addr) { return true; });
+    print_addr_list(ip_list, [](const IpAddr &ip_addr) { return ip_addr[0] == 1; });
+    print_addr_list(ip_list, [](const IpAddr &ip_addr) { return ip_addr[0] == 46 && ip_addr[1] == 70; });
+    print_addr_list(ip_list, [](const IpAddr &ip_addr) { return any_is_equal(ip_addr, 46); });
 }
