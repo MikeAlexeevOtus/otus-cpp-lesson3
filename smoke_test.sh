3b5f2b785b93e5018ea0776993311dd3b06f93c0
MikeAlexeevOtus <mike.42.alexeev@yandex.ru>
2020-01-25 13:58:38 +0300

    added smoke test

---
+++ smoke_test.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -eux
+set -o pipefail
+
+exe=$1
+
+DATA_FILE=ip_filter.tsv
+REFERENCE_VALUE=24e7a7b2270daee89c64d3ca5fb3da1a
+
+echo testing with "$exe"
+
+md5=$($exe < $DATA_FILE | md5sum | cut -d' ' -f1)
+
+test "$md5" == "$REFERENCE_VALUE"
+
+echo OK
