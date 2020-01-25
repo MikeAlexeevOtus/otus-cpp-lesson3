53c6c4d720e226ed99cd57a6c9e083787a6823b4
MikeAlexeevOtus <mike.42.alexeev@yandex.ru>
2020-01-25 14:19:12 +0300

    added deploy script

---
+++ deploy.sh
@@ -0,0 +1,17 @@
+set -eux
+
+PROJECT=$1
+PKG_NAME=$2
+BUILDNUMBER=$3
+BINTRAY_API_KEY=$4
+
+USER=mikealexeevotus
+BINTRAY_URL=https://api.bintray.com/content
+PUBLISH_ARGS='deb_distribution=trusty;deb_component=main;deb_architecture=amd64;publish=1'
+
+version="0.0.${BUILDNUMBER}"
+deb_file=${PKG_NAME}-${version}-Linux.deb
+
+curl -T "${deb_file}" \
+    -u "${USER}:${BINTRAY_API_KEY}" \
+    "${BINTRAY_URL}/{$USER}/${PROJECT}/${PKG_NAME}/${TRAVIS_BUILD_NUMBER}/${deb_file};${PUBLISH_ARGS}"
