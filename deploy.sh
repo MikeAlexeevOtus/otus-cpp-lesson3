27f3ef8b223a27410e40de991515f43a9fa53e69
MikeAlexeevOtus <mike.42.alexeev@yandex.ru>
2020-01-25 14:26:49 +0300

    simplified deploy

--- deploy.sh
+++ deploy.sh
@@ -2,15 +2,13 @@ set -eux
 
 PROJECT=$1
 PKG_NAME=$2
-BUILDNUMBER=$3
-BINTRAY_API_KEY=$4
 
 USER=mikealexeevotus
 REPONAME=otus-cpp
 BINTRAY_URL=https://api.bintray.com/content
 PUBLISH_ARGS='deb_distribution=trusty;deb_component=main;deb_architecture=amd64;publish=1'
 
-version="0.0.${BUILDNUMBER}"
+version="0.0.${TRAVIS_BUILD_NUMBER}"
 deb_file=${PKG_NAME}-${version}-Linux.deb
 
 curl -T "${deb_file}" \
